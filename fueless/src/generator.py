40d4f6b93ee90b5bacce8800a1d99a1c57ef79a8
Anastasia  Kryzhanovskaya <ucens@student.kit.edu>
2017-09-15 19:09:53 +0200

    jam separat

--- fueless/src/generator.py
+++ fueless/src/generator.py
@@ -1,22 +1,6 @@
-import random
 from enum import Enum
 
-class TrafficJam:
-    length = 0
-    avg_speed = 0
-    reason = 0
-    start_time = 0
-    end_time = 0
-    def __init__(self):
-        self.length = round(random.uniform(1, 15), 2)
-        self.avg_speed = round(random.uniform(2.5, 10), 2)
-        self.reason = random.choice(list(Reasons))
-        self.start_time = round(random.uniform(1, 15), 2)
-    def __str__(self):
-        return '[Length: %s]' % (self.length) + \
-               '[Average speed: %s]' % (self.avg_speed) +\
-               '[Reason: %s]' % (self.reason) + \
-               '[Start time: %s]' % (self.start_time)
+from src.traffic_jam import TrafficJam
 
 
 class Reasons(Enum):
