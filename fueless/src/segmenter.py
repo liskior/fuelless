36ec11c2c4a013bef3424601b914b4cc6450e82a
Steffen Gufler <s.gufler@online.de>
2017-09-15 21:53:43 +0200

    implemented route segmentation

--- fueless/src/segmenter.py
+++ fueless/src/segmenter.py
@@ -1,8 +1,21 @@
+import src.segment as seg
+
+SPEED = 120
+
 class Segmenter:
 
-
-    def get_segmented_route(self):
+    def get_segmented_route(self, route, traffic_jams):
         segments = []
+        current_pos = 0
+
+        for jam in traffic_jams:
+            start = jam.start
+            if (start != current_pos):
+                segments.append(seg.Segment(current_pos, start - current_pos, SPEED))
+            segments.append(seg.Segment(start, jam.length, jam.avg_speed, jam))
+            current_pos = jam.start + jam.length
+
+        if current_pos < route.length:
+            segments.append(seg.Segment(current_pos, route.length - current_pos, SPEED))
 
         return segments
-
