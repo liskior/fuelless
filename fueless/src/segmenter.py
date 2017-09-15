4febbd6927260d7874fa135ead9b71bd7cbd879d
Steffen Gufler <s.gufler@online.de>
2017-09-15 23:54:01 +0200

    simplified segmenter class (no overlapping traffic jams allowed anymor)

--- fueless/src/segmenter.py
+++ fueless/src/segmenter.py
@@ -12,6 +12,15 @@ class Segmenter:
             if i < traffic_jams[passed_jams].start or len(traffic_jams) == passed_jams:
                 segments.append(seg.Segment(i))
             else:
+                segments.append(seg.Segment(i, traffic_jams[passed_jams]))
+                if i >= traffic_jams[passed_jams].start + traffic_jams[passed_jams].length:
+                    passed_jams += 1
+
+
+        for i in range(route.length):
+            if i < traffic_jams[passed_jams].start or len(traffic_jams) == passed_jams:
+                segments.append(seg.Segment(i))
+            else:
                 for j in range(len(traffic_jams) - passed_jams):
                     if traffic_jams[j].start + traffic_jams[j].length >= i:
                         if len(segments) == i:
