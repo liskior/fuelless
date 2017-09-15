5c82519f636ddb6c950b3398a9da83c2f6b9e6c3
Steffen Gufler <s.gufler@online.de>
2017-09-15 19:15:22 +0200

    method definitions for segmenter

--- fueless/src/segment.py
+++ fueless/src/segment.py
@@ -1,2 +1,6 @@
 class Segment:
-    
+    def __init__(self, start, length, avg_speed, jam_info=None):
+        self.start = start
+        self.length = length
+        self.jam_info = jam_info
+        self.avg_speed = avg_speed
--- fueless/src/segmenter.py
+++ fueless/src/segmenter.py
@@ -1,3 +1,8 @@
-route = #TODO
-traffic_jams = []
+class Segmenter:
+
+
+    def get_segmented_route(self):
+        segments = []
+
+        return segments
 
