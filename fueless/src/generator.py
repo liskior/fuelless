76a5bf86d463b6bf06bf75d184a4d42347a8f352
Anastasia  Kryzhanovskaya <ucens@student.kit.edu>
2017-09-15 23:02:52 +0200

    jobannyi np array

--- fueless/src/generator.py
+++ fueless/src/generator.py
@@ -1,41 +1,31 @@
 import random
 
-from src.traffic_jam import TrafficJam
+from src.traffic_jam import TrafficJam, Reasons
 import numpy as np
 
 
 def get_jam():
     jam = TrafficJam()
-    a = np.array()
-    b = np.array()
-    a.append(jam.length)
-    a.append(jam.avg_speed)
-    a.append(jam.reason)
-    a.append(jam.start_time)
-    b.append(a)
-    b.append(round(random.uniform(1, 8), 2))
+    jam.length = round(random.uniform(1, 15), 2)
+    jam.avg_speed = round(random.uniform(2.5, 10), 2)
+    jam.reason = random.choice(list(Reasons))
+    jam.start_time = round(random.uniform(1, 15), 2)
+    a = np.array([jam.length, jam.avg_speed, jam.reason, jam.start_time])
+    b = np.array([a, round(random.uniform(1, 8), 2)])
     return b
 
 print(get_jam())
 
+
 def generate():
-<<<<<<< HEAD
-    c = np.array()
+    training_data = np.array(get_jam())
     for i in range(40000):
-        c.append(get_jam())
-    d = np.array()
+        np.concatenate((training_data, np.array(get_jam())))
+    validation_data = np.array(get_jam())
     for i in range(20000):
-        c.append(get_jam())
-    e = np.array()
-=======
-    training_data = []
-    for i in range(40000):
-        training_data.append(get_jam())
-    validation_data = []
-    for i in range(20000):
-        validation_data.append(get_jam())
-    test_data = []
->>>>>>> 74bf7d399231d4028357c7f6a9e9e5700af9e18a
+        np.concatenate((validation_data, np.array(get_jam())))
+    test_data = np.array(get_jam())
     for i in range(10000):
-        test_data.append(get_jam())
+        np.concatenate((test_data, np.array(get_jam())))
     return training_data, validation_data, test_data
+generate()
