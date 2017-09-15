ec287b751a783cbee8593aa99876b2f84e08b19b
Anastasia  Kryzhanovskaya <ucens@student.kit.edu>
2017-09-15 22:12:02 +0200

    time jam

--- fueless/src/generator.py
+++ fueless/src/generator.py
@@ -6,10 +6,12 @@ from src.traffic_jam import TrafficJam
 def get_jam() :
     jam = TrafficJam()
     a = []
+    b = []
     a.append(jam.length)
     a.append(jam.avg_speed)
     a.append(jam.reason)
     a.append(jam.start_time)
-    a.append(round(random.uniform(1, 8), 2))
-    return a
+    b.append(a)
+    b.append(round(random.uniform(1, 8), 2))
+    return b
 print(get_jam())
