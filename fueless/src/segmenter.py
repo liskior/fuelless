52c199fc92ef03ac445cbdc79fba5fa81348bc12
Steffen Gufler <s.gufler@online.de>
2017-09-15 23:58:38 +0200

    deleted trash

--- fueless/src/segmenter.py
+++ fueless/src/segmenter.py
@@ -15,19 +15,5 @@ class Segmenter:
                 segments.append(seg.Segment(i, traffic_jams[passed_jams]))
                 if i >= traffic_jams[passed_jams].start + traffic_jams[passed_jams].length:
                     passed_jams += 1
-
-
-        for i in range(route.length):
-            if i < traffic_jams[passed_jams].start or len(traffic_jams) == passed_jams:
-                segments.append(seg.Segment(i))
-            else:
-                for j in range(len(traffic_jams) - passed_jams):
-                    if traffic_jams[j].start + traffic_jams[j].length >= i:
-                        if len(segments) == i:
-                            segments.append(seg.Segment(i, traffic_jams[j]))
-                        else:
-                            segments[i].jam_info.append(traffic_jams[j])
-                    else:
-                        passed_jams += 1
         
         return segments
